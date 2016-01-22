8947ebb9a6b417f9171ffc11b1f5944599c9c2cb
TommyYehCool <tommy.yeh1112@gmail.com>
2016-01-22 09:53:20 +0800

    adjust code format

--- src/com/syscom/test/TestLengthToTwoBytes.java
+++ src/com/syscom/test/TestLengthToTwoBytes.java
@@ -5,9 +5,9 @@ public class TestLengthToTwoBytes {
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	
 	public static byte[] convertIntTo2Byte(int msgLen) {
-        byte[] bRet = new byte[2];
+		byte[] bRet = new byte[2];
         for (int i = 0; i < 2; i++) {
-            bRet[1 - i] = (byte)(msgLen >> (i * 8));
+        	bRet[1 - i] = (byte)(msgLen >> (i * 8));
         }
         return bRet;
     }
@@ -17,11 +17,11 @@ public class TestLengthToTwoBytes {
 	}
 	
 	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 	    }
 	    return new String(hexChars);
 	}
